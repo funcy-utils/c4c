f10b4f734bbe44e3f984ed2d4e8c54235d908669
t4ccer <64430288+t4ccer@users.noreply.github.com>
2021-01-29 22:29:56 +0100

    Add multi file support

--- src/C4C.hs
+++ src/C4C.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module C4C(run) where
 
 import           System.Environment
+import           System.Exit
 import qualified Data.Text.IO.Utf8 as UTF8
 import qualified Data.Text as T
 import Data.Text (Text)
@@ -8,21 +11,36 @@ import Data.Text (Text)
 import           C4C.Error
 import           C4C.Substitute
 import           C4C.Utils
+import Control.Monad (when)
+import Data.List (isPrefixOf)
 
 run :: [FilePath] -> IO ()
 run args
-  | [confFP, inpFP] <- args
+  | (confFP:fs) <- args
   = do
     config <- UTF8.readFile confFP
-    input  <- UTF8.readFile inpFP
-    let newFP = removeExt inpFP
-    case replaceContent config input of
-      Left e  -> print e
-      Right x -> UTF8.writeFile newFP x
+    let fpaths = filter (not . isPrefixOf "-") fs
+    let printPath = "--print-paths" `elem` fs
+    mapM_ (runOne config printPath) fpaths
+    when printPath (putStrLn "")
 run _ = do
   putStr "Usage: "
   getProgName >>= putStr
-  putStrLn " <config.conf> <filename.ext.c4c>"
+  putStrLn " <config.conf> [OPTIONS] <filename.ext.c4c>"
+  putStrLn "OPTIONS: "
+  putStrLn "\t--print-paths"
+
+runOne :: Text -> Bool -> FilePath -> IO ()
+runOne config printPath fpath = do
+  input  <- UTF8.readFile fpath
+  let newFP = removeExt fpath
+  case replaceContent config input of
+      Left e  -> do
+        print e
+        exitFailure 
+      Right x -> do
+        UTF8.writeFile newFP x
+        when printPath (putStr (newFP <> " "))
 
 replaceContent :: Text -> Text -> Either Error Text
 replaceContent config input = do
